492ba21ef807f836580a7529268c72dd87913f8a
Nyams <kellyjunior6386@gmail.com>
2025-03-17 07:01:13 +0300

    Initialising the frontend structure

---
+++ frontend/src/Home/index.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+    return (
+        <div className="fill" id="home-container">
+            <div className="logos">
+            </div>
+            <div className="main-container">
+                <div className="inner-content">
+                    <p>
+                        <strong>SuperTokens</strong> x <strong>React</strong> <br /> example project
+                    </p>
+                    <div className="buttons">
+                        <a href="/auth" className="sessionButton">
+                            Sign-up / Login
+                        </a>
+                        <a href="/dashboard" className="sessionButton">
+                            Dashboard
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
