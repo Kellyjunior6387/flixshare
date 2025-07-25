4844b55f78761d48cd9b8f388f0bc5cf49b133dc
Nyams <kellyjunior6386@gmail.com>
2025-07-25 15:06:41 +0300

    Modified the settings to use 127.0.0.1 rather than localhost.

--- backend/auth_services/auth_services/settings.py
+++ backend/auth_services/auth_services/settings.py
@@ -155,13 +155,13 @@ init(
 
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000"
+    "http://127.0.0.1:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://127.0.0.1:3000"
 ]
 
 CORS_ALLOW_HEADERS: List[str] = list(default_headers) + [
--- backend/room_management/room_management/settings.py
+++ backend/room_management/room_management/settings.py
@@ -146,13 +146,13 @@ init(
 )
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000"
+    "http://127.0.0.1:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://127.0.0.1:3000"
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
