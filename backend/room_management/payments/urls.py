acbd9a813b89f4183000d0cb0e170a8747347c1c
Nyams <kellyjunior6386@gmail.com>
2025-08-01 15:06:26 +0300

    I modified the callback URL to use my backend server forwarded on Ngrok. I also store the callback data in the database in the Transactions table"

--- backend/room_management/payments/urls.py
+++ backend/room_management/payments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MpesaSTKPushView
+from .views import MpesaSTKPushView, MpesaCallbackView, TransactionListView
 
 urlpatterns = [
     path('stk-push/', MpesaSTKPushView.as_view(), name='stk-push'),
+    path('callback/',  MpesaCallbackView.as_view(), name='callback'),
+    path('transactions/', TransactionListView.as_view(), name='transactions')
 ]
--- backend/room_management/payments/views.py
+++ backend/room_management/payments/views.py
@@ -7,17 +7,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from .models import Transaction
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.generics import ListAPIView
+from .serializers import TransactionSerializer
 
 
 class MpesaSTKPushView(APIView):
     permission_classes = [AllowAny]
     def post(self, request):
-        phone = request.data.get("phone")
+        phone = request.data.get("phone_number")
         amount = request.data.get("amount")
         room = request.data.get("room")
 
         if not phone or not amount or not room:
-            return Response({"error": "Missing required fields"}, status=400)
+            return Response({"error": "Missing required fields"}, status=status.HTTP_400_BAD_REQUEST)
 
         # Step 1: Generate Access Token
         data_to_encode = f"{settings.MPESA_CONSUMER_KEY}:{settings.MPESA_CONSUMER_SECRET}"
@@ -46,7 +51,7 @@ class MpesaSTKPushView(APIView):
             "PhoneNumber": phone,
             "CallBackURL": settings.MPESA_CALLBACK_URL,
             "AccountReference": 'room',
-            "TransactionDesc": "Payment of"+ room
+            "TransactionDesc": f"Payment of {room}"
         }
 
         stk_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
@@ -54,3 +59,51 @@ class MpesaSTKPushView(APIView):
 
 
         return Response(response.json(), status=response.status_code)
+@method_decorator(csrf_exempt, name='dispatch')
+class MpesaCallbackView(APIView):
+    authentication_classes = [] 
+    permission_classes=[AllowAny]
+    def post(self, request):
+        if request.method != "POST":
+            return Response({"error":"Only POST allowed"}, status=405)
+        try:
+            data = json.loads(request.body)
+            stk_callback = data.get("Body", {}).get("stkCallback", {})
+
+            result_code = stk_callback.get("ResultCode")
+            result_desc = stk_callback.get("ResultDesc")
+            metadata = stk_callback.get("CallbackMetadata", {}).get("Item", []) if result_code == 0 else []
+
+            amount = MpesaReceiptNumber = phone_number = transaction_date = None
+            if result_code == 0:
+                for item in metadata:
+                    if item["Name"] == "Amount":
+                        amount = item['Value']
+                    elif item["Name"] == "MpesaReceiptNumber":
+                        MpesaReceiptNumber = item['Value']
+                    elif item['Name'] == 'PhoneNumber':
+                        phone_number = item['Value']
+                    elif item['Name'] == 'TransactionDate':
+                        transaction_date = item['Value']
+            Transaction.objects.create(
+                phone_number=phone_number or 'Unknown',
+                amount=amount or 0,
+                MpesaReceiptNumber=MpesaReceiptNumber or 'N/A',
+                status='SUCCESS',
+                description=result_desc,
+                timestamp=transaction_date
+            )
+            return Response({'message': 'Callback processed'}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            return Response({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
+class TransactionListView(ListAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Transaction.objects.all().order_by('timestamp')
+    serializer_class = TransactionSerializer
+    
